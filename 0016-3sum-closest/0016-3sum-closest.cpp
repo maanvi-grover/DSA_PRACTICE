3f614084d45828acad8c3de494eb957ae2e27fe1
maanvi-grover <9r6rbnwvrc@privaterelay.appleid.com>
2026-09-11 19:19:24 +0530

    Time: 15 ms (30.71%), Space: 14 MB (50.63%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+
+        int closest_sum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n-2; i++) {
+            int left = i+1;
+            int right = n-1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                //check if sum is closer to target
+                if (abs(sum - target) < abs(closest_sum - target)) {
+                    closest_sum = sum;
+                }
+
+                //exact match
+                if (sum == target) {
+                    return sum;
+                }
+
+                //two pointer logic
+                if (sum < target) {
+                    left++;
+                }
+                if (sum > target) {
+                    right--;
+                }
+            }
+        }
+            return closest_sum;
+    }
+};
